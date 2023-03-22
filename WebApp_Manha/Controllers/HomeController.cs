5fa53205506576c93512b420f465f5b8f0d1110b
JORGE BORDINASSO <jorgebordinasso@gmail.com>
2023-03-22 10:27:42 -0300

    ASP.NET

--- WebApp_Manha/Controllers/HomeController.cs
+++ WebApp_Manha/Controllers/HomeController.cs
@@ -35,5 +35,10 @@ namespace WebApp_Manha.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
