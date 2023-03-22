74f1d5ef5e67d5fb141088c5dc2edd814fa63b35
JORGE BORDINASSO <jorgebordinasso@gmail.com>
2023-03-22 08:52:41 -0300

    Add project files.

---
+++ WebApp_Manha/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp_Manha.Models;
+
+namespace WebApp_Manha.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Action Result devolve uma tela (html)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // mais uma tela no sistema
+        public IActionResult BemVindo()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
