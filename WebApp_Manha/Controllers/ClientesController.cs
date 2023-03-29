d868ed8d3292af0aff4016538bbb447f226c7835
JORGE BORDINASSO <jorgebordinasso@gmail.com>
2023-03-29 10:59:02 -0300

    lista de Cadastro

---
+++ WebApp_Manha/Controllers/ClientesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Manha.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone)
+
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "o campo nome não pode estar em branco";
+            }
+
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "o campo telefone não pode estar em branco";
+            }
+            return View();
+        }
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+    }
+}
