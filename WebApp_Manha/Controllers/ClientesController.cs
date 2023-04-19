291c983cf36b1ce9cc4337672041bb8fc4d08cb1
JORGE BORDINASSO <jorgebordinasso@gmail.com>
2023-04-19 10:48:19 -0300

    atualização

--- WebApp_Manha/Controllers/ClientesController.cs
+++ WebApp_Manha/Controllers/ClientesController.cs
@@ -5,31 +5,43 @@ namespace WebApp_Manha.Controllers
 {
     public class ClientesController : Controller
     {
+        public static List<ClienteViewModel> lista = new List<ClienteViewModel>();
         public IActionResult Lista()
         {   
-            ClienteViewModel novo = new ClienteViewModel();
-            novo.Nome = "Jorge Henrique";
-            novo.Id = 10;
-            novo.Telefone = "16997777947";
+            //ClienteViewModel novo = new ClienteViewModel();
+            //novo.Nome = "Jorge Henrique";
+            //novo.Id = 10;
+            //novo.Telefone = "16997777947";
 
-            ClienteViewModel novo2 = new ClienteViewModel();
-            novo.Nome = "Pedro";
-            novo.Id = 11;
-            novo.Telefone = "16997777947";
+            //ClienteViewModel novo2 = new ClienteViewModel();
+            //novo2.Nome = "Pedro";
+            //novo2.Id = 11;
+            //novo2.Telefone = "16997777947";
 
-            ClienteViewModel novo3 = new ClienteViewModel();
-            novo.Nome = "Fernando";
-            novo.Id = 12;
-            novo.Telefone = "16997777947";
+            //ClienteViewModel novo3 = new ClienteViewModel();
+            //novo3.Nome = "Fernando";
+            //novo3.Id = 12;
+            //novo3.Telefone = "16997777947";
 
 
-            List<ClienteViewModel> lista = new List<ClienteViewModel>();
-            lista.Add(novo);
-            lista.Add(novo2);
-            lista.Add(novo3);
+            
+            //lista.Add(novo);
+            //lista.Add(novo2);
+            //lista.Add(novo3);
 
             return View(lista);
         }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClienteViewModel model)
+        {
+            lista.Add(model);
+            return RedirectToAction("Lista");
+        }
+
+
+
+      
 
         public IActionResult Cadastro()
         {
